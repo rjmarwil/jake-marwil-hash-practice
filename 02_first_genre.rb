4209fe54564e388659ddfe44e8d83c2412c6fbd4
Jake Marwil <rjm02006@gmail.com>
2015-02-11 09:28:33 -0700

    list the first genre that each person listed

--- 02_first_genre.rb
+++ 02_first_genre.rb
@@ -1,3 +1,4 @@
 require_relative 'people'
 
 # What is the first genre that each person listed?
+puts PEOPLE["joe"][:preferences][:favorite_genres].first, PEOPLE["sue"][:preferences][:favorite_genres].first
