4d76e316637e6ec9518afe79e47262194d514940
Jake Marwil <rjm02006@gmail.com>
2015-02-12 10:15:22 -0700

    print last name of joe's neighbor

--- 03_joes_neighbor.rb
+++ 03_joes_neighbor.rb
@@ -3,3 +3,6 @@ require_relative 'people'
 # What is the last name of the person sitting next to joe?
 #
 # HINT: Start by finding who joe is sitting next to, then find that person in the hash.
+neighbor = PEOPLE["joe"][:sitting_next_to]
+
+puts PEOPLE[neighbor.to_s][:last_name]
